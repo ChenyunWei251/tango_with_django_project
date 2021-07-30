a6f064739757cbdd22042b8bdcb17929010c5410
Chenyun Wei <2519997w@student.gla.ac.uk>
2021-07-30 13:59:58 +0800

    session data

--- rango/views.py
+++ rango/views.py
@@ -21,10 +21,10 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    context_dict['visits'] = int(request.COOKIES.get('visits', '1'))
+    visitor_cookie_handler(request)
+    context_dict['visits'] = request.session['visits']
 
     response = render(request, 'rango/index.html', context=context_dict)
-    visitor_cookie_handler(request, response)
     return response
 
 
@@ -147,6 +147,13 @@ def user_logout(request):
     return redirect(reverse('rango:index'))
 
 
+def get_server_side_cookie(request, cookie, default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
+
+
 def visitor_cookie_handler(request, response):
     visits = int(request.COOKIES.get('visits', '1'))
     last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
@@ -154,7 +161,9 @@ def visitor_cookie_handler(request, response):
                                         '%Y-%m-%d %H:%M:%S')
     if (datetime.now() - last_visit_time).days > 0:
         visits = visits + 1
-        response.set_cookie('last_visit', str(datetime.now()))
+        # Update the last visit cookie now that we have updated the count
+        request.session['last_visit'] = str(datetime.now())
     else:
-        response.set_cookie('last_visit', last_visit_cookie)
+        # Set the last visit cookie
+        request.session['last_visit'] = last_visit_cookie
     response.set_cookie('visits', visits)
